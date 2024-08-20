2378e59372b8e4cfa87fd992d4c535302fb6a07a
Abhinimap <prajapatabhishek@nimapinfotech.com>
2024-08-20 10:30:08 +0530

    changes

--- lib/internet_checker.dart
+++ lib/internet_checker.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:flutter/material.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
